bad7d963503bca37eb88f41235c6e757e8e2ce2c
isabeenk <sabeen.fatima0697@gmail.com>
2020-12-16 15:59:47 +0500

    topic names and values

--- src/app/containers/pages/add-new-product-modal/add-new-product-modal.component.ts
+++ src/app/containers/pages/add-new-product-modal/add-new-product-modal.component.ts
@@ -14,10 +14,32 @@ export class AddNewProductModalComponent {
     class: 'modal-right'
   };
   categories = [
-    {label: 'Cakes', value: 'chocolate'}
-    {label: 'Health and wellness', value: 'health&wellness'}
-
-
+    {label: 'Cakes', value: 'chocolate'},
+    {label: 'Wallpapers', value: 'wallpapers'},
+    {label: 'Nature', value: 'nature'},
+    {label: 'People', value: 'people'},
+    {label: 'Architecture', value: 'architecture'},
+    {label: 'Current Events', value: 'current events'},
+    {label: 'Experimental', value: 'experimental'},
+    {label: 'Fashion', value: 'fashion'},
+    {label: 'Film', value: 'film'},
+    {label: 'Health and wellness', value: 'healthandwellness'},
+    {label: 'Interiors', value: 'interiors'},
+    {label: 'Street Photography', value: 'streetphotography'},
+    {label: 'Work From Home', value: 'work from home'},
+    {label: 'Technology', value: 'technology'},
+    {label: 'Travel', value: 'travel'},
+    {label: 'Textures and Patterns', value: 'texturesandpatterns'},
+    {label: 'Business and Work', value: 'businessandwork'},
+    {label: 'COVID-19', value: 'covid19'},
+    {label: 'Animals', value: 'animals'},
+    {label: 'Food and Drinks', value: 'foodanddrinks'},
+    {label: 'Athletics', value: 'athletics'},
+    {label: 'Spirituality', value: 'spirituality'},
+    {label: 'Food and Drinks', value: 'foodanddrinks'},
+    {label: 'Arts and Culture', value: 'artsandculture'},
+    {label: 'History', value: 'history'},
+    {label: 'Sustainability', value: 'sustainability'},
   ];
   items
   callback

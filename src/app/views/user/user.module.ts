6884c0262a652187eedff1e78cf5476f5577aeec
isabeenk <sabeen.fatima0697@gmail.com>
2020-12-17 18:04:13 +0500

    register form

--- src/app/views/user/user.module.ts
+++ src/app/views/user/user.module.ts
@@ -5,11 +5,12 @@ import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { UserComponent } from './user.component';
 import { UserRoutingModule } from './user.routing';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SimpleNotificationsModule } from 'angular2-notifications';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ComponentsStateButtonModule } from 'src/app/components/state-button/components.state-button.module';
+import {AlertModule} from 'ngx-bootstrap/alert';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ForgotPasswordComponent, UserComponent, ResetPasswordComponent],
@@ -19,7 +20,9 @@ import { ComponentsStateButtonModule } from 'src/app/components/state-button/com
     FormsModule,
     SharedModule,
     SimpleNotificationsModule.forRoot(),
-    ComponentsStateButtonModule
+    ComponentsStateButtonModule,
+    ReactiveFormsModule,
+    AlertModule
   ]
 })
 export class UserModule { }
